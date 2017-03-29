430b88b6e3a4b2611dac163d3049ed69a3f4db33
kraeml <kraeml@users.noreply.github.com>
2017-03-29 20:58:39 +0200

    Python build

--- install_python.sh
+++ install_python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script name:     install_python.sh
-# last modified:   2016/12/29
+# last modified:   2017/03/22
 # sudo:            yes
 #
 # see: http://sowingseasons.com/blog/building-python-3-4-on-raspberry-pi-2.html
@@ -10,14 +10,28 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
+#Python 3 version to install
+version="3.6.1"
+
 #------------------------------------------------------
-apt-get install -y \
-  python3 \
-  python3-dev \
-  python3-pip \
-  zlib1g-dev \
-  libjpeg-dev
+apt-get install -y build-essential libncursesw5-dev
+apt-get install -y libgdbm-dev libc6-dev
+apt-get install -y zlib1g-dev libsqlite3-dev tk-dev
+apt-get install -y libssl-dev openssl
+apt-get install -y libreadline-dev libbz2-dev
 #------------------------------------------------------
 
-# No update while apt-get install pip
-# pip3 install -U pip
+wget "https://www.python.org/ftp/python/$version/Python-$version.tgz"
+tar zxvf "Python-$version.tgz"
+cd "Python-$version"
+./configure
+make
+make install
+pip3 install pip --upgrade
+
+# clean up
+
+cd ..
+
+rm -rf "./Python-$version"
+rm "./Python-$version.tgz"
