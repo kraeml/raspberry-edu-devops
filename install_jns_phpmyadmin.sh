363d8b64ef690d93b26ec14ec2d4149e5ca65f5e
kraeml <kraeml@sesamestreet.franken.de>
2017-03-31 21:56:37 +0200

    Change zip in tar.gz phpmyadmin file

--- install_jns_phpmyadmin.sh
+++ install_jns_phpmyadmin.sh
@@ -5,7 +5,7 @@
 
 cd /tmp
 version="4.7.0"
-wget https://files.phpmyadmin.net/phpMyAdmin/${version}/phpMyAdmin-${version}-all-languages.zip
+wget https://files.phpmyadmin.net/phpMyAdmin/${version}/phpMyAdmin-${version}-all-languages.tar.gz
 tar xvf phpMyAdmin-${version}-all-languages.tar.gz
 sudo mv phpMyAdmin-${version}-all-languages /var/www/html/phpMyAdmin
 sudo chown -R www-data: /var/www/html/phpMyAdmin/
