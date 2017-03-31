c75fb1419e947ecd128b0af4ad85ce6e337ce795
kraeml <kraeml@sesamestreet.franken.de>
2017-03-31 07:47:37 +0200

    Install mysql skript added

---
+++ install_jns_mysql.sh
@@ -0,0 +1,11 @@
+MYSQL_ROOT_PASSWORD=root
+echo "Install software"
+sleep 45
+echo "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+sudo apt-get update
+sudo apt install -y mysql-server
+
+
+sudo sed -i 's/bind-address[[:space:]]*=[[:space:]]*127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo service mysql restart
