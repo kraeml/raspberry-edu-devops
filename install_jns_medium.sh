4889ec2379c50c2182e1597f52b2511065e633f3
Kraeml <kraeml@sesamestreet.franken.de>
2017-04-05 19:52:42 +0000

    Update install_jns_medium.sh

--- install_jns_medium.sh
+++ install_jns_medium.sh
@@ -7,14 +7,28 @@ if [ $(id -u) = 0 ]; then
    echo "to be run without sudo"
    exit 1
 fi
+sudo chown -R pi: .
+# Additional scripts to be executed on the first boot after install.
+# This makes the `raspbian-ua-netinst` installer more uniform and easier
+# to maintain regardless of the use.
+if [ ! -e $HOME/.firstboot ]; then
+  echo -n "First boot detected on "
+  date
+  # run scripts
+  ./install_jns_fast.sh
+  sudo ./install_tex.sh
+  sudo ./install_dependencies.sh
+  ./install_nodered.sh
+  ./install_cloud9.sh
+  ./install_docker.sh
+  ./install_jns_php7.sh
+  ./install_jns_mysql.sh
+  ./install_jns_phpmyadmin.sh
 
-# run scripts
-./install_jns_fast.sh
-sudo ./install_tex.sh
-sudo ./install_dependencies.sh
-./install_nodered.sh
-./install_cloud9.sh
-./install_docker.sh
-./install_jns_php7.sh
-./install_jns_mysql.sh
-./install_jns_phpmyadmin.sh
+  touch $HOME/.firstboot
+
+  sudo shutdown -r +1 "First boot installation completed. Please log off now."
+  echo -n "First boot installation completed on "
+  date
+fi
+
