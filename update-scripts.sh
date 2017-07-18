bcf02497d0a25fe240dfcb7971fed3584a541ecd
kraeml <kraeml@sesamestreet.franken.de>
2017-07-18 21:51:56 +0200

    Insert Comments and refactoring in update-scripts.sh

--- update-scripts.sh
+++ update-scripts.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
-ME=$(whoami)
+# ME=$(whoami)
 
+# Which branch should be loaded
 branch=$(cat $HOME/.raspberry-edu-devops.branch)
+# Load and merge new version
 git pull
+# Download objects from repro
 git fetch origin
+# Switch to branch and reset do origin. Remove untracked directories/files
 git checkout $branch && git reset --hard origin/$branch && git clean -f -d
 
-# Set permissions
-chmod -R 744 *
+# Set permissions for shell skripts
+chmod -R 744 *.sh
