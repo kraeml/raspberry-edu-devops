92b27f91aa2c85fc41a81c4a10b77b87254680fb
kraeml <kraeml@sesamestreet.franken.de>
2017-07-20 15:32:22 +0200

    PhpMyAdmin with cookie passwort

--- install_jns_phpmyadmin.sh
+++ install_jns_phpmyadmin.sh
@@ -8,5 +8,6 @@ version="4.7.2"
 wget https://files.phpmyadmin.net/phpMyAdmin/${version}/phpMyAdmin-${version}-all-languages.tar.gz
 tar xvf phpMyAdmin-${version}-all-languages.tar.gz
 sudo mv phpMyAdmin-${version}-all-languages /var/www/html/phpMyAdmin
+sudo cp config.inc.php /var/www/html/phpMyAdmin/
 sudo chown -R www-data: /var/www/html/phpMyAdmin/
 rm phpMyAdmin-${version}-all-languages.tar.gz
