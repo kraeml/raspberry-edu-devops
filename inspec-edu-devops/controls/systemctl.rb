30607de7732606b110dc3106cf98bf1cf9b21edd
kraeml <kraeml@sesamestreet.franken.de>
2017-07-22 13:56:05 +0200

    Adding inspec test for python php ansible inspec

--- inspec-edu-devops/controls/systemctl.rb
+++ inspec-edu-devops/controls/systemctl.rb
@@ -10,6 +10,21 @@ services = [
     "cloud9",
     "mysql"
 ]
+ansible = { :version => '2.2.1.0', :command => 'ansible --version'}
+python3 = { :version => '3.6.2', :command => 'python3 --version'}
+inspec = { :version => '1.31.1', :command => 'inspec --version'}
+php = { :version => '7.0.19', :command => 'php --version'}
+
+tools = [ ansible, python3, inspec, php ]
+control 'packages-1.0' do
+    impact 1.0
+    title 'Ensure packages installed'
+    tools.each do |tool|
+        describe command(tool[:command]) do
+          its(:stdout) { should match /tool[:version]/ }
+        end
+    end
+end
 # you add controls here
 control 'systemctl-1.0' do                        # A unique ID for this control
   impact 1.0                                # The criticality, if this control fails.
