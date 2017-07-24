21530caff41cb699988243ee7c3dfc32c94a87fe
kraeml <kraeml@sesamestreet.franken.de>
2017-07-24 09:37:33 +0200

    Set ans without spaces

--- install_inspec.sh
+++ install_inspec.sh
@@ -12,7 +12,7 @@ sudo apt-get update
 # sudo apt-get install --yes ruby ruby-dev gcc make
 wget https://gist.githubusercontent.com/blacktm/8302741/raw/b498c27c03e2b7dcf79ca00183b6ccdf361b7001/install_ruby_rpi.sh
 chmod u+x install_ruby_rpi.sh
-sed -i 's/read -p "  Continue? (y\/n) " ans/ans = "y"/g' install_ruby_rpi.sh
+sed -i 's/read -p "  Continue? (y\/n) " ans/ans="y"/g' install_ruby_rpi.sh
 ./install_ruby_rpi.sh
 source ~/.bashrc
 gem update system
