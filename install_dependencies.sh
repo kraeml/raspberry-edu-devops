477db00d05fd0925bf89fa38a52fb3ae7afdc5ce
kraeml <kraeml@sesamestreet.franken.de>
2017-08-15 14:16:27 +0200

    Dependencies without lcx

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -18,8 +18,9 @@ apt-get install -y nmap curl sudo expect
 # Todo: Change for python3.6
 apt-get install -y sense-hat rpi.gpio
 apt-get install -y debian-keyring debian-archive-keyring
-apt-get install -y dphys-swapfile lxc lsb-core
-mkdir -p /var/lxc/guests/test
-debootstrap jessie /var/lxc/guests/test/fs/ http://archive.raspbian.org/raspbian
+apt-get install -y dphys-swapfile lsb-core
+#apt-get install -y lxc
+#mkdir -p /var/lxc/guests/test
+#debootstrap jessie /var/lxc/guests/test/fs/ http://archive.raspbian.org/raspbian
 #apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
 #apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7638D0442B90D010
