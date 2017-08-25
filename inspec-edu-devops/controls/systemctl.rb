311ce54d4245a29b794ade00e10a0cb0b574ef33
kraeml <kraeml@sesamestreet.franken.de>
2017-08-25 22:48:40 +0200

    Adding inspec test for pyvirtualdisplay and geckodriver

--- inspec-edu-devops/controls/systemctl.rb
+++ inspec-edu-devops/controls/systemctl.rb
@@ -238,7 +238,14 @@ tools = {
     :pip3_selenium => {
         :version => '3.5.0',
         :command => 'pip3 show selenium'
-    }
+    };
+    :geckodriver => {
+        :version => '0.18.0';
+        :command => 'geckodriver --version'
+    },
+    :pyvirtualdisplay => {
+        :version => '0.2',
+        :command => 'pip3 show pyvirtualdisplay'
 }
 
 control 'gem packages' do
