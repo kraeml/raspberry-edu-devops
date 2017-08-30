64ab742a8a9da50c063b2c6fe118e96e765a34a5
kraeml <kraeml@sesamestreet.franken.de>
2017-08-30 14:28:58 +0200

    inspec skript with capybara

--- inspec_edu.sh
+++ inspec_edu.sh
@@ -3,3 +3,7 @@
 # sudo:            no
 node-red-admin login
 LC_MESSAGES=C sudo bash -l -c 'inspec exec /home/pi/.raspberry-edu-devops/inspec-edu-devops'
+
+cd /home/pi/.raspberry-edu-devops/capybara-edu-devops
+bundle exec cucumber 
+cd -
