5de84b0a3d82db046836545847b0adaa5f9e8226
kraeml <kraeml@sesamestreet.franken.de>
2017-11-07 22:42:11 +0100

    Process without k in notebook

--- inspec-edu-devops/controls/systemctl.rb
+++ inspec-edu-devops/controls/systemctl.rb
@@ -127,7 +127,7 @@ tools = {
         :version => '4.4',
         :command => 'jupyter --version',
         :port => '8888',
-        :process => 'jupyter-notebook'
+        :process => 'jupyter-noteboo' # This is right without k
     },
     :pip3_jupyter => {
         :version => '1.0',
