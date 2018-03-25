ebbcc9b650c68298b57b75b500436f70342e91d4
kraeml <kraeml@sesamestreet.franken.de>
2018-03-25 20:23:19 +0200

    Workaround for ansible 2.5 error

--- install_ansible.sh
+++ install_ansible.sh
@@ -12,7 +12,7 @@ echo "Install ansible via pip"
 sudo apt-get install -y libffi-dev libssl-dev cowsay sshpass ieee-data
 
 #ToDo kerberos dependencies
-ANSIBLE="ansible pywinrm enum34 httplib2 idna ipaddress jinja2 crypto cryptography markupsafe netaddr paramiko pyasn1 six xmltodict"
+ANSIBLE="'ansible>=2.4.0,<2.5.0' pywinrm enum34 httplib2 idna ipaddress jinja2 crypto cryptography markupsafe netaddr paramiko pyasn1 six xmltodict"
 sudo pip3 install $ANSIBLE  || (sudo apt-get install python3-pip && sudo pip3 install $ANSIBLE)
 
 sudo mkdir -p /etc/ansible
