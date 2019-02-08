ecbdd3529e9e126210b0e6462d24cedc04a87c74
kraeml <kraeml@sesamestreet.franken.de>
2019-02-08 14:07:47 +0100

    Improvement for version

--- inspec-edu-devops/controls/systemctl.rb
+++ inspec-edu-devops/controls/systemctl.rb
@@ -102,49 +102,59 @@ users = {
 tools = {
     :ansible => {
         :version => '2.7.1',
+        :version_command => '',
         :command => 'ansible --version'
     },
     :python3 => {
         :version => '3.5.3',
+        :version_command => '',
         :command => 'python3 --version'
     },
     :php => {
         :version => '7.0.30',
+        :version_command => '',
         :command => 'php --version' },
     :mysql => {
         :version => '10.1',
+        :version_command => '',
         :command => '/usr/sbin/mysqld --version |sed "s/  / /g" | cut -d \  -f 3',
         :port => '3306',
         :process => 'mysqld'
     },
     :nginx => {
         :version => '1.10',
+        :version_command => '',
         :command => '/usr/sbin/nginx -v 2>&1 | cut -d / -f 2'#,
         #:port => '80',
         #:process => 'nginx'
     },
     :jupyter => {
         :version => '4.4',
+        :version_command => '',
         :command => 'jupyter --version',
         :port => '8888',
         :process => 'jupyter-noteboo' # This is right without k
     },
     :pip3_jupyter => {
         :version => '1.0',
+        :version_command => '',
         :command => 'pip3 show jupyter'
     },
     # Not included in hypriot
     #:pip3_readline => {
     #    :version => '6.2',
+    #    :version_command => '',
     #    :command => 'pip3 show readline'
     #},
     # Not included in hypriot
     #:pip3_ipyparallel => {
     #    :version => '6.0',
+    #    :version_command => '',
     #    :command => 'pip3 show ipyparallel'
     #},
     :pip3_bash_kernel => {
         :version => '0.7.1',
+        :version_command => '',
         :command => 'pip3 show bash_kernel'
     },
     :jupyter_bash_kernel => {
@@ -158,6 +168,7 @@ tools = {
     # Not included in hypriot
     #:jupyter_javascript => {
     #    :version => 'javascript',
+    #    :version_command => '',
     #    :command => 'jupyter-kernelspec list'
     #},
     :jupyter_widgets => {
@@ -167,20 +178,24 @@ tools = {
     # Not included in hypriot
     #:pip3_ipython_sql => {
     #    :version => '0.3',
+    #    :version_command => '',
     #    :command => 'pip3 show ipython-sql'
     #},
     :nodered => {
         :version => '0.19',
+        :version_command => '',
         :command => 'node-red --help',
         :port => '1880',
         :process => 'node-red'
     },
     :nodejs => {
         :version => '8.0',
+        :version_command => '',
         :command => 'nodejs -v'
     },
     :npm => {
         :version => '6.0',
+        :version_command => '',
         :command => 'npm -v'
     },
     :cloud9 => {
@@ -193,108 +208,130 @@ tools = {
     },
     :pillow => {
         :version => '4.0',
+        :version_command => '',
         :command => 'pip3 show pillow'
     },
     # On hypriot the stretch packages will installed
     #:rpigpio => {
     #    :version => '0.6',
+    #    :version_command => '',
     #    :command => 'pip3 show rpi.gpio'
     #},
     :sensehat => {
         :version => '2.2',
+        :version_command => '',
         :command => 'pip3 show sense-hat'
     },
     # On hypriot the stretch packages will installed
     #:blinkt => {
     #    :version => '0.1',
+    #    :version_command => '',
     #    :command => 'pip3 show blinkt'
     #},
     :requests => {
         :version => '2.12',
+        :version_command => '',
         :command => 'pip3 show requests'
     },
     :psutil => {
         :version => '5.4',
+        :version_command => '',
         :command => 'pip3 show psutil'
     },
 
     :pahomqtt => {
         :version => '1.4',
+        :version_command => '',
         :command => 'pip3 show paho-mqtt'
     },
     :tweepy => {
         :version => '3.5',
+        :version_command => '',
         :command => 'pip3 show tweepy'
     },
     :noderedrpigpio => {
         :version => '0.19',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
-        :command => 'node-red-admin info node-red/rpi-gpio | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red/rpi-gpio | grep -i version'
     },
     :bigtimer => {
         :version => '2.0',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
-        :command => 'node-red-admin info node-red-contrib-bigtimer | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red-contrib-bigtimer | grep -i version'
     },
     :dashboard => {
         :version => '2.10',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
-        :command => 'node-red-admin info node-red-dashboard | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red-dashboard | grep -i version'
     },
     :looptimer => {
         :version => '0.0.8',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
-        :command => 'node-red-admin info node-red-contrib-looptimer | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red-contrib-looptimer | grep -i version'
     },
     :noderedsensehat => {
         :version => '0.0.18',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
-        :command => 'node-red-admin info node-red-node-pi-sense-hat | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red-node-pi-sense-hat | grep -i version'
     },
     :noderedopcua => {
         :version => '0.2.32',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
-        :command => 'node-red-admin info node-red-contrib-opcua | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red-contrib-opcua | grep -i version'
     },
     :noderedopcuaiiot => {
         :version => '3.3.0',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
-        :command => 'node-red-admin info node-red-contrib-iiot-opcua | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red-contrib-iiot-opcua | grep -i version'
     },
     :noderedsolarpowerforecast => {
         :version => '1.0.3',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
-        :command => 'node-red-admin info node-red-contrib-solar-power-forecast | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red-contrib-solar-power-forecast | grep -i version'
     },
     :noderedopenweathermap => {
         :version => '0.2.1',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
-        :command => 'node-red-admin info node-red-node-openweathermap | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red-node-openweathermap | grep -i version'
     },
     :noderedblinkt => {
         :version => '0.19',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
-        :command => 'node-red-admin info node-red/rpi-blinkt | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red/rpi-blinkt | grep -i version'
     },
     :noderedexplorehat => {
         :version => '0.19',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'disabled',
-        :command => 'node-red-admin info node-red/rpi-explorerhat | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red/rpi-explorerhat | grep -i version'
     },
     :noderedflotilla => {
         :version => '0.19',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'disabled',
-        :command => 'node-red-admin info node-red/node-flotilla | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red/node-flotilla | grep -i version'
     },
 #    :noderedledborg => {
 #        :version => '0.17',
+#        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
 #        :commend => 'disabled',
-#        :command => 'node-red-admin info node-red-node-ledborg/ledborg | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+#        :command => 'node-red-admin info node-red-node-ledborg/ledborg | grep -i version'
 #    },
     :noderedunicorn => {
         :version => '0.19',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'disabled',
-        :command => 'node-red-admin info node-red/rpi-unicorn | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red/rpi-unicorn | grep -i version'
     },
     :mongodb => {
         :version => '2.4',
@@ -306,52 +343,64 @@ tools = {
     },
     :noderedmongo => {
         :version => '0.0.14',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
         :command => 'node-red-admin info node-red-node-mongodb/mongo | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \   '
     },
     :noderedmysql => {
         :version => '0.0.17',
+        :version_command => ' | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  ',
         :commend => 'enabled',
-        :command => 'node-red-admin info node-red-node-mysql/mysql | grep -i version | cut -d : -f 2 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr -d \  '
+        :command => 'node-red-admin info node-red-node-mysql/mysql | grep -i version'
     },
     #:lxc => {
     #    :version => '1.0',
+    #    :version_command => '',
     #    :command => 'lxc-create --version'
     #},
     :abiword => {
         :version => '3.0',
+        :version_command => '',
         :command => '/usr/bin/abiword --version'
     },
     :libreoffice => {
         :version => '5.2',
+        :version_command => '',
         :command => '/usr/bin/libreoffice --version'
     },
     :phantomjs => {
         :version => '2.1',
+        :version_command => '',
         :command => 'phantomjs --version'
     },
     :pip3_selenium => {
         :version => '3.14.1',
+        :version_command => '',
         :command => 'pip3 show selenium'
     },
     :geckodriver => {
         :version => '0.18.0',
+        :version_command => '',
         :command => 'geckodriver --version'
     },
     :pyvirtualdisplay => {
         :version => '0.2',
+        :version_command => '',
         :command => 'pip3 show pyvirtualdisplay'
     },
     :wiringpi => {
         :version => '2.46',
+        :version_command => '',
         :command => 'gpio -v'
     },
     :gem => {
         :version => '2.5',
+        :version_command => '',
         :command => 'gem -v'
     },
     :tidy => {
         :version => '5.2',
+        :version_command => '',
         :command => 'tidy --version'
     },
     :java => {
@@ -425,14 +474,16 @@ control 'packages-1.0' do
     title 'Ensure packages installed'
     tools.each do |key, value|
         if value[:command]
-            describe command(value[:command]) do
-              if value[:version]
-                  its(:stdout) { should cmp >= value[:version] }
-              end
-              if value[:commend]
-                  its(:stdout) { should match /#{value[:commend]}/ }
-              end
+          if value[:version]
+            describe command(value[:command] + value[:version_command]) do
+              its(:stdout) { should cmp >= value[:version] }
             end
+          end
+          if value[:commend]
+            describe command(value[:command]) do
+              its(:stdout) { should match /#{value[:commend]}/ }
+            end
+          end
         end
     end
 end
