5d5a88fe187ff166b374182aa617dcc78ecadd1a
kraeml <kraeml@sesamestreet.franken.de>
2019-02-08 19:57:17 +0100

    Swap inspec with  cucumber test

--- inspec_edu.sh
+++ inspec_edu.sh
@@ -2,7 +2,6 @@
 # script name:     inspec_edu.sh
 # sudo:            no
 node-red-admin login
-LC_MESSAGES=C sudo bash -l -c "inspec exec /home/$USER/.raspberry-edu-devops/inspec-edu-devops"
 
 cd /home/$USER/.raspberry-edu-devops/capybara-edu-devops
 sudo sed -i 's/jns:jns$/:/' /lib/systemd/system/cloud9.service
@@ -13,3 +12,5 @@ sudo sed -i 's/:$/jns:jns/' /lib/systemd/system/cloud9.service
 sudo systemctl daemon-reload
 sudo systemctl restart cloud9.service
 cd -
+
+LC_MESSAGES=C sudo bash -l -c "inspec exec /home/$USER/.raspberry-edu-devops/inspec-edu-devops"
