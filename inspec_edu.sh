4c2d2ad624e25396afe0137016e1622dfd7b387f
kraeml <kraeml@sesamestreet.franken.de>
2019-09-24 21:56:06 +0200

    Disable cucumber test

--- inspec_edu.sh
+++ inspec_edu.sh
@@ -3,14 +3,14 @@
 # sudo:            no
 node-red-admin login
 
-cd /home/$USER/.raspberry-edu-devops/capybara-edu-devops
-sudo sed -i 's/jns:jns$/:/' /lib/systemd/system/cloud9.service
-sudo systemctl daemon-reload
-sudo systemctl restart cloud9.service
-bundle exec cucumber
-sudo sed -i 's/:$/jns:jns/' /lib/systemd/system/cloud9.service
-sudo systemctl daemon-reload
-sudo systemctl restart cloud9.service
-cd -
+#cd /home/$USER/.raspberry-edu-devops/capybara-edu-devops
+#sudo sed -i 's/jns:jns$/:/' /lib/systemd/system/cloud9.service
+#sudo systemctl daemon-reload
+#sudo systemctl restart cloud9.service
+#bundle exec cucumber
+#sudo sed -i 's/:$/jns:jns/' /lib/systemd/system/cloud9.service
+#sudo systemctl daemon-reload
+#sudo systemctl restart cloud9.service
+#cd -
 
 LC_MESSAGES=C sudo bash -l -c "inspec exec /home/$USER/.raspberry-edu-devops/inspec-edu-devops"
